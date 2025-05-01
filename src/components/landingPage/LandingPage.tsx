ef4c14fd362d6d4c7fe4507fbc2748c72ab5c12a
moso0x <mosokimothon@gmail.com>
2025-05-01 10:22:15 +0300

    added the hove effects on the cards, added image logo from postImage

--- src/components/landingPage/LandingPage.tsx
+++ src/components/landingPage/LandingPage.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Rocket} from 'lucide-react';
 import { title } from 'process';
 import { info } from 'console';
-// import logo from ""
+ import logo from "../Comments/"
 
 
 
@@ -12,16 +12,17 @@ const LandingPage = () => {
     <>
 
     
-  
+<img className='w-[100px] mx-auto' src="https://i.postimg.cc/mrmNqjP5/logo.jpg" alt="My image" />
+
 
   {/* //TOP SECTION */}
 
     <div className='grid items-center text-center justify-around gap-4 '>
         {/* <img src={logo} alt="" /> */}
-      <p className='text-sm text-[#ef9c60] my-6 '>Bitcoin Script VM in Cairo</p>
+      <p className='text-sm text-[#ef9c60] mt-2 mb-6 '>Bitcoin Script VM in Cairo</p>
       <h1 className='text-5xl text-gray-100'>Revolutionizing Code Sharing in Web3</h1>
       <h2 className='text-gray-200 text-4xl my-4'>With Blockchain-powered Collaboration</h2>
-      <button className='my-20 flex  pt-2 mx-auto justify-center bg-[#36a7b9] text-3xl w-[300px] h-[50px] gap-2 rounded-full'>Let's get started<Rocket className='w-8 h-8' /></button>
+      <button className='my-20 flex  pt-2 mx-auto justify-center bg-[#36a7b9] text-3xl w-[300px] h-[50px] gap-2 rounded-full transition-transform duration-300 ease-in-out transform hover:scale-105'>Let's get started<Rocket className='w-8 h-8' /></button>
       <p className='text-gray-400 w-[77%] justify-center text-center text-xl mx-auto mb-4'>Snipu is a Decentralized Code snippet Hub built on Starknet That Redefines How Developers Store, share, and Collacorate on Code. By leveraging Blockchain Technology. The platform Ensures That code Snippets are stored securely On-chain  While offering seamless acces to aglobal Community  Of Developers. Users Aurthenticate with popular Starknet wallets Such as Bravos and Argent, Manage their persoonall snippets, And Share Them With ease Through Sharable links. </p>
       <hr className='w-[50%] mx-auto my-4' />
     </div>
@@ -30,7 +31,7 @@ const LandingPage = () => {
 <div className='my-20 '>
   <h1 className='mx-auto text-5xl  text-center font-bold text-[#ef9c60] my-[4rem]'>Key Features</h1>
  <div className='grid mx-auto grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
-  <div className='bg-gradient-to-b from-[#1a2127] to-[#1a1a1a] mx-auto p-8 rounded-3xl w-[500px]' >
+  <div className='bg-gradient-to-b from-[#1a2127] to-[#1a1a1a] mx-auto p-8 rounded-3xl w-[500px] transition-transform duration-300 ease-in-out transform hover:scale-105' >
     <h1 className='text-3xl text-gray-100 underline'>Core EVM Implementation</h1>
     <ul className=' text-gray-400 text-2xl py-4 list-disc pl-5'>
       <li> Full Implementation Of Bitcoin Script Opcodes In Cairo</li>
@@ -42,7 +43,7 @@ const LandingPage = () => {
   </div>
 
 
-  <div className='bg-gradient-to-b from-[#1a2127] to-[#1a1a1a] mx-auto p-8 rounded-3xl w-[500px]' >
+  <div className='bg-gradient-to-b from-[#1a2127] to-[#1a1a1a] mx-auto p-8 rounded-3xl w-[500px] ransition-transform duration-300 ease-in-out transform hover:scale-105' >
     <h1 className='text-3xl text-gray-100 underline'>Script Compilation</h1>
     <ul className=' text-gray-400 text-2xl py-4 list-disc pl-5'>
       <li> Bitcoin script to cairo Transpilation with Optimization</li>
@@ -52,7 +53,7 @@ const LandingPage = () => {
     </ul>
 
   </div>
-  <div className='bg-gradient-to-b from-[#1a2127] to-[#1a1a1a] mx-auto p-8 rounded-3xl w-[500px]' >
+  <div className='bg-gradient-to-b from-[#1a2127] to-[#1a1a1a] mx-auto p-8 rounded-3xl w-[500px] ransition-transform duration-300 ease-in-out transform hover:scale-105' >
     <h1 className='text-3xl text-gray-100 underline'>Verification Tools</h1>
     <ul className=' text-gray-400 text-2xl py-4 list-disc pl-5'>
       <li>Zero-knowledge Proof forscript Execution</li>
@@ -62,7 +63,7 @@ const LandingPage = () => {
     </ul>
 
   </div>
-  <div className='bg-gradient-to-b from-[#1a2127] to-[#1a1a1a] mx-auto p-8 rounded-3xl w-[500px]' >
+  <div className='bg-gradient-to-b from-[#1a2127] to-[#1a1a1a] mx-auto p-8 rounded-3xl w-[500px] ransition-transform duration-300 ease-in-out transform hover:scale-105' >
     <h1 className='text-3xl text-gray-100 underline'>Developer Tools</h1>
     <ul className=' text-gray-400 text-2xl py-4 list-disc pl-5'>
       <li>Comprehensive API for integrating Snipu into existing applications</li>
@@ -72,7 +73,7 @@ const LandingPage = () => {
     </ul>
 
   </div>
-  <div className='bg-gradient-to-b from-[#1a2127] to-[#1a1a1a] mx-auto p-8 rounded-3xl w-[500px]' >
+  <div className='bg-gradient-to-b from-[#1a2127] to-[#1a1a1a] mx-auto p-8 rounded-3xl w-[500px] ransition-transform duration-300 ease-in-out transform hover:scale-105' >
     <h1 className='text-3xl text-gray-100 underline'>Interoperability Features</h1>
     <ul className=' text-gray-400 text-2xl py-4 list-disc pl-5'>
       <li> Cross-chain messaging capabilities using bitcoin Scripts</li>
@@ -82,7 +83,7 @@ const LandingPage = () => {
     </ul>
 
   </div>
-  <div className='bg-gradient-to-b from-[#1a2127] to-[#1a1a1a] mx-auto p-8 rounded-3xl w-[500px]' >
+  <div className='bg-gradient-to-b from-[#1a2127] to-[#1a1a1a] mx-auto p-8 rounded-3xl w-[500px] ransition-transform duration-300 ease-in-out transform hover:scale-105' >
     <h1 className='text-3xl text-gray-100 underline'>Security Component</h1>
     <ul className=' text-gray-400 text-2xl py-4 list-disc pl-5'>
       <li> formal Verification of core VM components</li>
